323bc361ca09ec56fed03c05051f31238240b2a4
Alexandre <alesimar@gmail.com>
2012-04-09 18:55:29 -0400

    Built the skeleton. Added the messagelist and postmessage activities

---
+++ Client/src/tmp/test/pkg/postmessage.java
@@ -0,0 +1,16 @@
+package tmp.test.pkg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class postmessage extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    setContentView(R.layout.postmessage);
+	}
+
+}
