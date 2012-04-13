998129849c0e02f7f5fe8aff05d1359efd349543
Alexandre <alesimar@gmail.com>
2012-04-13 18:12:38 -0400

    login button redirects to messagelist

--- Client/src/tmp/test/pkg/login.java
+++ Client/src/tmp/test/pkg/login.java
@@ -1,16 +1,38 @@
 package tmp.test.pkg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.login);
+
+		Button login = (Button)this.findViewById(R.id.btnLogin);
+		login.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				SwitchToMessageList();
+			}
+		});
+
+	}
 	
-	    setContentView(R.layout.login);
+	private void SwitchToMessageList()
+	{
+		Intent intent = new Intent(this, messagelist.class);
+		//intent.putExtra("curr_lang", this.lang);
+		this.startActivityForResult(intent, 0);
 	}
 
 }
